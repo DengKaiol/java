5f3c49e91c8bbc599d8887c7c97d042edd1fb611
DengKai <1206286987@qq.com>
2022-11-02 23:21:20 +0800

    OOP-lab

--- OOP-lab/src/main/java/Run.java
+++ OOP-lab/src/main/java/Run.java
@@ -1,8 +1,9 @@
 public class Run {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.print("aaa");
     }
-    public int add(int a, int b){
-        return a+b;
+
+    public int add(int a, int b) {
+        return a + b;
     }
 }
